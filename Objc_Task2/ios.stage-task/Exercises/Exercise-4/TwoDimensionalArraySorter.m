890003501fc9ddf024c24f5793df43baa2326b93
ILLIA NEMYTSKA <78763502+NEMYTSKA@users.noreply.github.com>
2021-05-15 13:40:22 +0300

    fix: fix 1

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -5,32 +5,43 @@
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
     
      NSMutableArray *arrayString = [NSMutableArray new];
-       NSMutableArray *arrayNumber = [NSMutableArray new];
-       
-       for (NSArray *i in array) {
-           if (![i isKindOfClass:NSArray.class]) {
-               return @[];
-           }
-           
-           for (id j in i) {
-               if ([j isKindOfClass:NSString.class]) {
-                   [arrayString addObject:j];
-               }
-               if ([j isKindOfClass:NSNumber.class]) {
-                   [arrayNumber addObject:j];
-               }
-           }
-       }
-       
-       [arrayString sortUsingSelector:@selector(compare:)];
-       [arrayNumber sortUsingSelector:@selector(compare:)];
-       
-       NSMutableArray *resultArray = [NSMutableArray arrayWithArray:arrayNumber];
-       [resultArray addObjectsFromArray:arrayString];
-       
-       
-       
-       return resultArray;
+     NSMutableArray *arrayNumber = [NSMutableArray new];
+     
+     for (NSArray *i in array) {
+         if (![i isKindOfClass:NSArray.class]) {
+             return @[];
+         }
+         
+         for (id j in i) {
+             if ([j isKindOfClass:NSString.class]) {
+                 [arrayString addObject:j];
+             }
+             if ([j isKindOfClass:NSNumber.class]) {
+                 [arrayNumber addObject:j];
+             }
+         }
+     }
+     
+     [arrayString sortUsingSelector:@selector(compare:)];
+     [arrayNumber sortUsingSelector:@selector(compare:)];
+     
+     
+     if (arrayString.count != 0 && arrayNumber.count != 0) {
+         NSMutableArray *reversStringArray = [NSMutableArray arrayWithArray:[[arrayString reverseObjectEnumerator] allObjects]];
+         NSMutableArray *resultArray = [NSMutableArray arrayWithObjects:arrayNumber,reversStringArray, nil];
+         return resultArray;
+     }
+     
+     NSMutableArray *resultArray = [NSMutableArray new];
+     
+     if (arrayString.count == 0) {
+         [resultArray addObjectsFromArray:arrayNumber];
+     }
+     if (arrayNumber.count == 0) {
+         [resultArray addObjectsFromArray:arrayString];
+     }
+
+     return resultArray;
 }
 
 @end
